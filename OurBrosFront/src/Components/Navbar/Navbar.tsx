bb1deb9e81f8fa9b777ef102e5fb7eaa12f85b51
SteveLorde <mostafa.maher98@gmail.com>
2023-07-24 21:38:34 +0200

    work on navbar, frontend app

---
+++ OurBrosFront/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+﻿import "./Navbar.css"
+import reactLogo from './assets/react.svg'
+
+export function Navbar() {
+    return (
+        <div className="nav">
+            <div className="navbody">
+                <ul>
+                    
+                </ul>
+                <svg></svg>
+            </div>
+        </div>
+    );
+}
---
+++ OurBrosFront/src/Data/Models/Lobby.ts
@@ -0,0 +1,8 @@
+﻿import {User} from "./User.ts";
+
+export interface Lobby {
+    users: User[]
+    datetime: number
+    lobbyadmin: User
+    
+}
