32df7b2490048c3f6b0f0bf82690a293d398296a
SteveLorde <mostafa.maher98@gmail.com>
2023-07-27 18:52:04 +0200

    created Lobbies controller, interface for database

---
+++ OurBrosAPI/Controllers/Lobbies.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OurBrosAPI.Services.Database;
+
+namespace OurBrosAPI.Controllers;
+
+[ApiController]
+public class Lobbies : Controller
+{
+    //inject IDatabase service
+    private IDatabase _db;
+
+    public Lobbies(IDatabase db)
+    {
+        _db = db;
+    }
+    // GET
+    [HttpGet(Name = "GetLobbies")]
+    public IActionResult GetLobbies()
+    {
+        
+        return Ok();
+    }
+    
+    [HttpGet(Name = "ShitLobby")]
+    public IActionResult LOLLobbies()
+    {
+        
+        return Ok();
+    }
+    
+    
+}
--- OurBrosAPI/Services/Chat/Hubs/ChatHub.cs
+++ OurBrosAPI/Services/Chat/Hubs/ChatHub.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using OurBrosAPI.Data.Models;
 
 namespace OurBrosAPI.Services.Chat.Hubs;
 
 public class ChatHub : Hub
 {
+    private readonly string _botUser;
+
+    public ChatHub()
+    {
+        _botUser = "MyChat Bot";
+    }
+    public async Task JoinLobby(UserConnection userconnection)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, userconnection.Lobby);
+            
+        await Clients.Group(userconnection.Lobby).SendAsync("ReceiveMessage", _botUser, $"{userconnection.user} has joined {userconnection.Lobby}");
+    }
+    
+    
+    /*
     public async Task SendMessage(string user,string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
@@ -13,6 +29,6 @@ public class ChatHub : Hub
     {
         
     }
-    
+    */
     
 }
